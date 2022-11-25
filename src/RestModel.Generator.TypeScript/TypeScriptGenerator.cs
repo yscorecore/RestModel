568ef3154579aa7e69a2f0ab36f2731064d24c2d
pengbo <obpt@sina.com>
2022-11-25 15:03:16 +0800

    add projects

---
+++ src/RestModel.Generator.TypeScript/TypeScriptGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection.Emit;
+
+namespace RestModel.Generator.TypeScript
+{
+    public class TypeScriptGenerator : IGenerator
+    {
+        public async Task Generator(GeneratorContext context)
+        {
+            await this.GeneratModels(context);   
+        }
+        private Task GeneratModels(GeneratorContext context)
+        { 
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/RestModel/IGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestModel
+{
+    public interface IGenerator
+    {
+        Task Generator(GeneratorContext context);
+    }
+    public class GeneratorContext
+    {
+        public AppInfo App { get; set; }
+    }
+}
